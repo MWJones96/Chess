92f68107bbaca15bef7da04c41e88ae2ee8081cc
Matthew Jones <mj317@emps-mac03.ex.ac.uk>
2017-10-02 17:27:15 +0100

    Done Knight, Bishop, Rook and Queen

--- src/Pieces/Knight.java
+++ src/Pieces/Knight.java
@@ -17,6 +17,11 @@ public class Knight extends Piece
     {
         ArrayList<Move> moves = new ArrayList<Move>();
 
+        Game g = Game.getInstance();
+
+        int x = m_pos[0];
+        int y = m_pos[1];
+
         Move[] m = new Move[]{new Move(2, 1),
                               new Move(2, -1),
                               new Move(1, -2),
@@ -26,12 +31,48 @@ public class Knight extends Piece
                               new Move(1, 2),
                               new Move(-1, 2)};
 
+        if(m_color == Color.WHITE) {
+            for (int i = 0; i < m.length; i++) {
+                if (x + m[i].m_x < 0 || x + m[i].m_x > g.m_board.m_size - 1 ||
+                        y + m[i].m_y < 0 || y + m[i].m_y > g.m_board.m_size - 1) {
+                    continue;
+                }
+
+                if (g.m_board.m_pieces[x + m[i].m_x][y + m[i].m_y] != null) {
+                    if (g.m_board.m_pieces[x + m[i].m_x][y + m[i].m_y].m_color == Color.BLACK) {
+                        moves.add(new Move(x + m[i].m_x, y + m[i].m_y));
+                        continue;
+                    }
+                } else {
+                    moves.add(new Move(x + m[i].m_x, y + m[i].m_y));
+                }
+            }
+        }
+        else if(m_color == Color.BLACK)
+        {
+            for (int i = 0; i < m.length; i++) {
+                if (x + m[i].m_x < 0 || x + m[i].m_x > g.m_board.m_size - 1 ||
+                        y + m[i].m_y < 0 || y + m[i].m_y > g.m_board.m_size - 1) {
+                    continue;
+                }
+
+                if (g.m_board.m_pieces[x + m[i].m_x][y + m[i].m_y] != null) {
+                    if (g.m_board.m_pieces[x + m[i].m_x][y + m[i].m_y].m_color == Color.WHITE) {
+                        moves.add(new Move(x + m[i].m_x, y + m[i].m_y));
+                        continue;
+                    }
+                } else {
+                    moves.add(new Move(x + m[i].m_x, y + m[i].m_y));
+                }
+            }
+        }
 
         return moves;
     }
 
     public void move(int x, int y)
     {
-
+        Game g = Game.getInstance();
+        g.m_board.putPiece(x, y, this);
     }
 }
--- src/Pieces/Rook.java
+++ src/Pieces/Rook.java
@@ -13,13 +13,72 @@ public class Rook extends Piece
         super(color, pos, type, 'R');
     }
 
-    public ArrayList<Move> getMoves()
-    {
-        return null;
+    public ArrayList<Move> getMoves() {
+        ArrayList<Move> moves = new ArrayList<Move>();
+        Game g = Game.getInstance();
+
+        Move[] grads = new Move[]{new Move(1, 0),
+                new Move(0, 1),
+                new Move(-1, 0),
+                new Move(0, -1)};
+
+        if (m_color == Color.WHITE) {
+            for (int i = 0; i < 4; i++) {
+                int x = m_pos[0];
+                int y = m_pos[1];
+                while (true) {
+                    x += grads[i].m_x;
+                    y += grads[i].m_y;
+
+                    if (x < 0 || x > g.m_board.m_size - 1 || y < 0 || y > g.m_board.m_size - 1)
+                        break;
+
+                    if (g.m_board.m_pieces[x][y] == null) {
+                        moves.add(new Move(x, y));
+                    } else {
+                        if (g.m_board.m_pieces[x][y].m_color == Color.BLACK) {
+                            moves.add(new Move(x, y));
+                            break;
+                        } else if (g.m_board.m_pieces[x][y].m_color == Color.WHITE) {
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+        else if (m_color == Color.BLACK) {
+            for (int i = 0; i < 4; i++) {
+                int x = m_pos[0];
+                int y = m_pos[1];
+
+                while (true) {
+                    x += grads[i].m_x;
+                    y += grads[i].m_y;
+
+                    if (x < 0 || x > g.m_board.m_size - 1 || y < 0 || y > g.m_board.m_size - 1)
+                        break;
+
+                    if (g.m_board.m_pieces[x][y] == null) {
+                        moves.add(new Move(x, y));
+                    } else {
+                        if (g.m_board.m_pieces[x][y].m_color == Color.WHITE) {
+                            moves.add(new Move(x, y));
+                            break;
+                        } else if (g.m_board.m_pieces[x][y].m_color == Color.BLACK) {
+                            break;
+                        }
+                    }
+                }
+            }
+
+        }
+
+        return moves;
     }
 
     public void move(int x, int y)
     {
-
+        Game g = Game.getInstance();
+        g.m_board.putPiece(x, y, this);
     }
 }
